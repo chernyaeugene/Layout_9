a0fcaedf850518524aab61b352dd900ab4260297
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-09-09 20:06:06 +0300

    final desktop

--- Js.js
+++ Js.js
@@ -1,11 +1,15 @@
 // Слайдер
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper_menu, .swiper_gallery', {
     loop : true,
     speed: 800,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
+});
+new Swiper('.swiper_comments ', {
+    loop : true,
+    speed: 800,
     pagination: {
         el: '#swiper_comments',
         clickable: true,
