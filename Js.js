2237278f998ec979cadb029ea0b31d532c019d65
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-09-10 21:57:16 +0300

    adaptation

--- Js.js
+++ Js.js
@@ -15,3 +15,13 @@ new Swiper('.swiper_comments ', {
         clickable: true,
     },
 });
+// Бургер
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.header_menu_content');
+if (iconMenu){
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
