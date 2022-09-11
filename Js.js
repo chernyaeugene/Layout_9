9b5f4d7881eda5a62f1c8909c3323305b218e7d8
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-09-11 19:07:39 +0300

    finish adaptive

--- Js.js
+++ Js.js
@@ -1,13 +1,40 @@
 // Слайдер
-new Swiper('.swiper_menu, .swiper_gallery', {
+new Swiper('.swiper_menu', {
     loop : true,
     speed: 800,
+    spaceBetween: 15,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+        },
+        765: {
+            slidesPerView: 2,
+        },
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
 });
-new Swiper('.swiper_comments ', {
+new Swiper('.swiper_gallery', {
+    loop : true,
+    speed: 800,
+    spaceBetween: 20,
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    direction: 'vertical',
+    breakpoints: {
+        880: {
+            direction: 'horizontal',
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+});
+new Swiper('.swiper_comments', {
     loop : true,
     speed: 800,
     pagination: {
